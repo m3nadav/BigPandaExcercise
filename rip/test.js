71d85314a47f5fb38676481a7eb6456756776161
Nadav Mermer <nadav@mermers.net>
2015-10-16 17:20:49 +0300

    Finished getting and sending the github status. moving to angular

--- rip/test.js
+++ rip/test.js
@@ -4,67 +4,49 @@ var querystring = require('querystring');
 
 var app = express();
 var host = 'status.github.com';
+var apiJSON = '/api.json';
 
-function performRequest(endpoint, method, data, success) {
-  var dataString = JSON.stringify(data);
-  var headers = {};
-  
-  if (method == 'GET') {
-    endpoint += '?' + querystring.stringify(data);
-  }
-  else {
-    headers = {
-      'Content-Type': 'application/json',
-      'Content-Length': dataString.length
-    };n
-  }
-  var options = {
-    host: host,
-    path: endpoint,
-    method: method,
-    headers: headers
-  };
+function makeRequest(endpoint, process) {
+	var options = {
+		host: host,
+		path: endpoint,
+		method: 'GET'
+	};
 
-  var req = https.request(options, function(res) {
-    res.setEncoding('utf-8');
-
-    var responseString = '';
-
-    res.on('data', function(data) {
-      var jsonData = JSON.parse(data);
-      console.log(jsonData);
-      success(data);
-    });
-  });
-
-  req.write(dataString);
-  req.end();
+	var req = https.request(options, function(res) {
+		var responseString = '';
+		res.on('data', function(data) {
+			var jsonData = JSON.parse(data);
+			console.log(jsonData);
+			process(jsonData);
+		});
+	});
+	req.end();
 };
 
 app.get('/', function(req, res) {
-	performRequest('/api.json', 'GET', '', function(data) {
+	makeRequest(apiJSON, function(data) {
 		var statusUrl = data.status_url;
 		var messagesUrl = data.messages_url;
 		var lastMessageUrl = data.last_message_url;
-		performRequest(statusUrl, 'GET', '', function(date) {
+		makeRequest(statusUrl, function(data) {
 			var currentStatus = data;
-			performRequest(messagesUrl, 'GET', '', function(data) {
+			makeRequest(messagesUrl, function(data) {
 				if (data == '') {
-					performRequest(lastMessageUrl, 'GET', '', function(data) {
+					makeRequest(lastMessageUrl, function(data) {
 						var lastMessage = data;
-						res.send(currentStatus+"<BR>"+lastMessage);
+						var fullResponse = {currentStatus: currentStatus, recentMessages: [lastMessage]};
+						res.send(fullResponse);
 					});
 				} 
 				else {
-					res.send(currentStatus+"<BR>"+data);
+					var recentMessages = data;
+					var fullResponse = {currentStatus: currentStatus, recentMessages: [recentMessages]};
+					res.send(fullResponse);
 				};
-				
 			});
-		});
-		
+		});	
 	});
 });
-
-
 
 var server = app.listen(8888);
